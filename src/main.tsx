d9f5b8a60056bfbbb14c4d5cf2ee620215585b49
JackyrDC <djackyrcalix@gmail.com>
2024-12-02 16:56:03 -0600

    fix: 🐛 adding missing dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from './App';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
-console.log(import.meta.env.VITE_API_BASE_URL)
+console.log(import.meta.env.VITE_USERS_API_URL);
 root.render(
   <React.StrictMode>
     <App />
--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -1,8 +1,30 @@
-import {CapacitorHttp} from "@capacitor/core"
+import {CapacitorHttp,Capacitor} from "@capacitor/core"
+import {from, Observable, BehaviorSubject} from "rxjs"
 
 export class AuthService {
     login(username: string, password: string) {
-        const baseUrl = import.meta.env.VITE_API_BASE_URL+'/web/session/authenticate';
+        const baseUrl = import.meta.env.VITE_USERS_API_URL;
         
+        return from(CapacitorHttp.post({
+            url: baseUrl,
+            data:{
+                jsonrpc: "2.0",
+                method: "call",
+                params: {
+                    db: "prueba11",
+                    login: "prueba11@gmail.com",
+                    password: "hola123"
+                }
+            },
+            headers: {
+                'Content-Type': 'application/json'}}
+    ).pipe(
+        map((response)=>{
+            const user = response.data;
+        }
+
+        )
+    ))
+
     }
 }
