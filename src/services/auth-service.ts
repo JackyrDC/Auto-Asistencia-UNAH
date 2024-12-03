30ce4237f23dfa0539eb075413ade65c2d28861e
JackyrDC <djackyrcalix@gmail.com>
2024-12-03 10:46:20 -0600

    auth service added

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -1,8 +1,9 @@
 import {CapacitorHttp,Capacitor} from "@capacitor/core"
 import {from, Observable, BehaviorSubject} from "rxjs"
+import {map} from "rxjs/operators"
 
-export class AuthService {
-    login(username: string, password: string) {
+export class AuthService{
+    login(username: string, password: string) : Observable<any>{
         const baseUrl = import.meta.env.VITE_USERS_API_URL;
         
         return from(CapacitorHttp.post({
